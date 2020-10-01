f4310b7289b4c675c00f93add760fa3b7567bd4a
Xander Porter <Xander.p101@stu.nebo.edu>
2020-10-01 12:20:22 -0600

    I put the prototype 1( driving sim) into the alc game dev folder

---
+++ driving simulator prototype/Assets/scripts/followplayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followplayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset = new Vector3(0, 5, -7);
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+ }
